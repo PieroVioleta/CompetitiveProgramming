588ea840c0c7fac2066e05903d5f0bc23559a0d4
Piero Violeta Estrella <45082124+PieroVioleta@users.noreply.github.com>
2019-01-09 21:00:15 -0500

    :v Ad-Hoc

---
+++ codeforces/1077-Div3-A.cpp
@@ -0,0 +1,21 @@
+//Frog jumping
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[102];
+
+int main(){
+
+	int t;
+	scanf("%d", &t);
+  
+	for(int i = 0; i<t; ++i){
+		long long int a, b, k;
+		scanf("%lli %lli %lli",&a, &b, &k);
+		if(k%2==0) cout << (a-b)*k/2 << endl;
+		else cout << ((k+1)/2)*a - ((k-1)/2)*b << endl;
+	}
+  
+	return 0;
+}
