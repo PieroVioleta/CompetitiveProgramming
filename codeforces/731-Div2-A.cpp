765bf6ff8e478ec2bb0ad1955ce5e3bf4697121f
Piero Violeta Estrella <45082124+PieroVioleta@users.noreply.github.com>
2019-01-16 15:01:04 -0500

    Contest uni1

--- codeforces/731-Div2-A.cpp
+++ codeforces/731-Div2-A.cpp
@@ -2,22 +2,18 @@
 
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
-	int n;
-	scanf("%d", &n);
-	int a[n];
-	
-	for(int i = 0; i<n; i++){
-		scanf("%d", &a[i]);
+	int cont=0;
+	char s[102];
+	cin >> s;
+	int inicio;
+	if(abs('a'-s[0])<=13) inicio = abs('a'-s[0]);
+	else inicio = 26-abs('a'-s[0]);
+	for(int i=0; i<strlen(s)-1; i++){
+		if(abs(s[i]-s[i+1])<=13) cont+=(abs(s[i]-s[i+1]));
+		else cont+= 26-abs(s[i]-s[i+1]);
 	}
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n-1; j++){
-			if(a[j]>a[j+1]){
-				swap(a[j], a[j+1]);
-			}
-		}
-	}
-	for(int i=0; i<n; i++) cout << a[i] << " ";
+	cout << cont+inicio;
 	return 0;
 }
