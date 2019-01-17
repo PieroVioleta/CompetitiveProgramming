ad9d67df2505a4bfe028005dcdc2e10983757390
Piero Violeta Estrella <45082124+PieroVioleta@users.noreply.github.com>
2019-01-17 15:27:39 -0500

    :D

--- codeforces/144-Div2-A.cpp
+++ codeforces/144-Div2-A.cpp
@@ -4,11 +4,6 @@ using namespace std;
 
 int main() {
 
-	#ifndef	ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
-
 	int n, max, min, cmax, cmin; 
 	scanf("%d", &n);
 	int a[n];
