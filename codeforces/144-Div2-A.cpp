810ec5ddf61b5a59b9c8ea204a439dca1e163278
Piero Violeta Estrella <45082124+PieroVioleta@users.noreply.github.com>
2019-01-17 15:26:39 -0500

    :D

---
+++ codeforces/144-Div2-A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+	#ifndef	ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	int n, max, min, cmax, cmin; 
+	scanf("%d", &n);
+	int a[n];
+
+	for(int i = 0; i < n; i++){
+		scanf("%d", &a[i]);
+	}
+
+	for(int i=0; i<n; i++){
+		if(i==0) max = a[i];
+		else{
+			if(a[i]>=max) max = a[i];
+		}
+	}
+
+	for(int i=0; i<n; i++){
+		if(i==0) min = a[i];
+		else{
+			if(a[i] <= min) min = a[i];
+		}
+	}
+
+	for(int i = 0; i < n; i++){
+		if(a[i] == max){
+			cmax = i;
+			break;
+		}
+	}
+
+	for(int i = n; i >= 0; i--){
+		if(a[i] == min){
+			cmin = i;
+			break;
+		}
+	}
+
+	if(cmax > cmin) cout << n - cmin + cmax - 2;
+	else cout << n - cmin + cmax - 1;
+	return 0;
+}
